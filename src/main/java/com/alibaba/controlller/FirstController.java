5720bd696abf1b29d1eef9bf6626586944d52f23
柯颖 <wb-ky964716@alibaba-inc.com>
2021-06-24 11:02:18 +0800

    first commit

---
+++ src/main/java/com/alibaba/controlller/FirstController.java
@@ -0,0 +1,13 @@
+package com.alibaba.controlller;
+
+/**
+ * start
+ *
+ * @author keying
+ * @date 2021/6/24
+ */
+public class FirstController {
+    public static void main(String[] args) {
+        
+    }
+}
